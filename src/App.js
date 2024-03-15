2e7194c4161fddeec4ee076d268ce1351c4e8c8a
Mohan-Paranthaman <mohanhuzzain@gmail.com>
2024-03-15 16:50:51 +0530

    addind src

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './data.js';
+import './List.js';
+
+function App() {
+  return <div>
+         <h2>reminder project setup</h2>
+        </div>
+        <data.js     />
+        <List.js />
+  
+
+
+  
+}
+
+export default App;
